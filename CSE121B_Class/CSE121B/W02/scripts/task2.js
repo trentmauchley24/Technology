4ca7380fb86253e6f3b985bc62742cc7ab00a27f
trentmauchley24 <mau22011@byuiedu.edu>
2023-06-16 17:09:34 -0600

    js

---
+++ CSE121B_Class/CSE121B/W02/scripts/task2.js
@@ -0,0 +1,56 @@
+/* Lesson 2 */
+
+/* VARIABLES */
+
+ // Step 1: declare and instantiate a variable to hold your name
+let myName = "Trent Mauchley";
+
+// Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
+document.querySelector("#name").textContent =myName;
+
+// Step 3: declare and instantiate a variable to hold the current year
+let thisYear = 2023;
+
+// Step 4: place the value of the current year variable into the HTML file
+
+document.querySelector('#year').textContent = thisYear;
+// Step 5: declare and instantiate a variable to hold the name of your picture
+let myPhoto;
+
+// Step 6: copy your image into the "images" folder
+
+// Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
+document.querySelector("#img1").setAttribute(src, myPhoto)
+
+
+
+/* ARRAYS */
+
+// Step 1: declare and instantiate an array variable to hold your favorite foods
+let myFoods =["apples", "pasta", "muffins", "meat"];
+
+// Step 2: place the values of the favorite foods variable into the HTML file
+document.querySelector("#food").textContent =myFoods;
+
+// Step 3: declare and instantiate a variable to hold another favorite food
+
+
+// Step 4: add the variable holding another favorite food to the favorite food array
+
+
+// Step 5: repeat Step 2
+
+
+// Step 6: remove the first element in the favorite foods array
+
+
+// Step 7: repeat Step 2
+
+
+// Step 8: remove the last element in the favorite foods array
+
+
+// Step 7: repeat Step 2
+
+
+
